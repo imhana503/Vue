e2f3f9faaba8917d6220ec7681262cddd0d60a52
LHN <imhana503@gmail.com>
2024-07-15 17:06:22 +0900

    .

--- vuetify-project/src/components/ImageGallary.vue
+++ vuetify-project/src/components/ImageGallary.vue
@@ -3,33 +3,39 @@
 </script>
 
 <template>
-    <VRow
-        align="start"
-      >
-        <VCol
-            v-for="n in 100"
-            cols="4"
+    <VCard class="ma-5">
+        <VRow
+            align="start"
         >
-            <Vcard>
-                <VImg   
-                    :src="`https://picsum.photos/500/300?image=${n*5+10}`"
-                    aspect-ratio="1"
-                    cover
-                >
-                    <template v-slot:placeholder>
-                        <VRow
-                            class="align-center justify-center fill-height"
-                        >
-                            <v-progress-circular
-                            color="grey-lighten-4"
-                            indeterminate
-                            />
-                        </VRow>
-                    </template>
-                </VImg>
-            </Vcard>
-        </VCol>
-    </VRow>
+            <VCol
+                v-for="n in 200"
+                cols="4"
+                sm="3"
+                md="2"
+                lg="1"
+            >
+                <Vcard>
+                    <VImg   
+                        :src="`https://picsum.photos/500/300?image=${n*5+10}`"
+                        :lazy-src="`https://picsum.photos/10/6?image=${n*5+10}`"
+                        aspect-ratio="1"
+                        cover
+                    >
+                        <template v-slot:placeholder>
+                            <VRow
+                                class="align-center justify-center fill-height"
+                            >
+                                <v-progress-circular
+                                color="grey-lighten-4"
+                                indeterminate
+                                />
+                            </VRow>
+                        </template>
+                    </VImg>
+                </Vcard>
+            </VCol>
+        </VRow>2
+    </VCard>
 </template>
 
 <style>
--- vuetify-project/src/plugins/vuetify.js
+++ vuetify-project/src/plugins/vuetify.js
@@ -7,23 +7,23 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@/assets/test.css'
 
 // Composables
 import { createVuetify } from 'vuetify';
 
-const myTheme = {
-  dark: false,
-  colors: {
-    background: '#DDD8D4',
-  },
-}
+// const myTheme = {
+//   dark: false,
+//   colors: {
+//     background: '#DDD8D4',
+//   },
+// }
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  theme: {
-    //defaultTheme: 'dark',
-   
-  },
+  plugins: [
+
+  ]
 })
 
  /*
