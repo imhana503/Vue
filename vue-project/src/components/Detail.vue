867053a0751b3b5f167aaa5f6b421fc072c1acac
Lim Hana <imhana503@gmail.com>
2025-11-04 06:13:14 +0900

    vue-project

---
+++ vue-project/src/components/Detail.vue
@@ -0,0 +1,12 @@
+<template>
+    <section id="detail" class="container">
+        <h1>{{ data[$route.params.id].title }}</h1>
+      asda
+    </section>
+</template>
+
+<script setup>
+  const props = defineProps({
+    data: Array,
+  }) 
+</script>
---
+++ vue-project/src/components/TitlePage.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <section id="title">
+    <div class="container">
+      <div class="detail row">
+        <figure class="col">
+          <img src="../assets/images/title.png" alt="title">
+        </figure>
+        <div class="text col">
+          <h2>Hello,<br>I'm Doolee</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+#title {
+  .container {
+    $navbar_height: 56px; // navbar 높이값
+    height: 100vh;
+    padding: $navbar_height;
+
+    .row {
+      height: 100%;
+      flex-direction: row-reverse;
+    }
+  }
+
+}
+figure {
+  img {
+    width: 100%;
+  }
+}
+</style>
